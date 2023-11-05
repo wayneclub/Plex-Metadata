07bd5fd58f4e32813cb023c3b0e1a476924a1035
wayneclub <me@wayneclub.com>
2023-11-05 08:00:32 +0800

    update

--- utils/plex.py
+++ utils/plex.py
@@ -12,19 +12,23 @@ class Plex(object):
     """
 
     def __init__(self):
-       self.plex = self.connect_plex()
+        self.plex = self.connect_plex()
 
     def connect_plex(self):
         """Connect Plex"""
-        log.info(" + Connect plex server")
+        log.info(" + Connect to plex server")
         if config.plex['baseurl'] and config.plex['token']:
             return PlexServer(config.plex['baseurl'], config.plex['token'])
         elif config.plex['username'] and config.plex['password'] and config.plex['servername']:
-            account = MyPlexAccount(config.plex['username'], config.plex['password'])
+            account = MyPlexAccount(
+                config.plex['username'], config.plex['password'])
             return account.resource(config.plex['servername']).connect()
 
+        log.exit(" - Unable to connect to plex server!")
 
-    def plex_find_lib(self, lib_type, plex_title="", title=""):
+    def plex_find_lib(self, lib_type: str, plex_title: str = "", title: str = ""):
+        """Find plex library"""
+
         if plex_title:
             lib = self.plex.library.search(title=plex_title, libtype=lib_type)
         else:
@@ -35,17 +39,18 @@ class Plex(object):
 
         if len(lib) > 1:
             for index, data in enumerate(lib):
-                print(f"{index}: {data.title} ({data.year}) [{data.ratingKey}]")
+                log.info(
+                    f"{index}: {data.title} ({data.year}) [{data.ratingKey}]")
 
             correct_index = int(input("請選擇要改的編號："))
             lib = lib[correct_index]
         elif len(lib) == 1:
             lib = lib[0]
         else:
-            print(f"plex找不到{title}，請附上正確標題")
-            exit(1)
+            log.exit(f"plex找不到{title}，請附上正確標題")
 
         return lib
+
 
 if __name__:
     log = Logger.getLogger("Plex")
